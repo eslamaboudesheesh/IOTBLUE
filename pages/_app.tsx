1aecc27acdb409042b8132018871e9dca517886c
Eslam Aboudesheesh <t-emhussein@efg-hermes.com>
2021-11-11 08:10:07 +0200

    handel scss deployment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
-import '../styles/globals.scss'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Provider} from 'react-redux';
 import store from '../redux/store';
 import type { AppProps } from 'next/app'
 import Layout from '../component/layout/layout'
 import 'font-awesome/css/font-awesome.min.css'
+import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
