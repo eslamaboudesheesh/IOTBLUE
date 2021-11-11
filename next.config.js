e1363a9dbf61627cd7c5880353c3b7d497c13310
Eslam Aboudesheesh <t-emhussein@efg-hermes.com>
2021-11-11 08:04:51 +0200

    add config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const config = require('./config.json')
+const path = require('path')
 
 module.exports = {
   reactStrictMode: true,
   publicRuntimeConfig: {
     baseURL: config.baseURL,
     websiteURL: config.websiteURL  
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
   },
   images: {
     domains: [config.imagesDomain]
